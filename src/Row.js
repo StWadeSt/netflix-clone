f50d99a518c45125770103eb9cb27221ec2dae80
SStuurman <stuart@lateral.solutions>
2021-08-19 10:29:46 +0200

    added the trailer functionality. When the user clicks on one of the movies it plays a trailer from youtube

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import './Row.css';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_image_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLarge }) {
     const [ movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
 
     useEffect(() => {
         async function fetchData(){
@@ -16,8 +19,29 @@ function Row({ title, fetchUrl, isLarge }) {
         fetchData();
     }, [fetchUrl]);//if this bracket is empty this useEffect wil only run once.
 
+    const opts = {
+        height: "390",
+        wight: "100%",
+        playerVars: {
+            
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = (movie) => {
+          trailerUrl? setTrailerUrl("") 
+        : movieTrailer(movie?.name || "")
+        .then(url => {
+            const urlParams = new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get('v'));
+        })
+        .catch((error) => console.log(error))
+    };
+
+
     return (
         <div className="row"> {/*Container*/}
+            {trailerUrl && <YouTube className="trailer" videoId={trailerUrl} opts={opts} />}
             {/*Title */}
             <h2>{title}</h2>
 
@@ -26,6 +50,7 @@ function Row({ title, fetchUrl, isLarge }) {
                 {movies.map(movie =>(
                     <img 
                         key={movie.id}
+                        onClick={() => handleClick(movie)}
                         className={`movie ${isLarge && "row__movieLarge"}`}
                         src={`${base_image_url}${isLarge? movie.poster_path : movie.backdrop_path }`}
                         alt={movie.name} 
